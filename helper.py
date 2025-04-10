6fe93dbc8f014b2c7df7c84c9701df24fd59bc91
Andre Lessa <lessa.a.p@gmail.com>
2025-04-10 11:46:31 +0200

    Updated plots

--- helper.py
+++ helper.py
@@ -9,6 +9,46 @@ from xml.etree import ElementTree as ET
 
 
 
+texDict = {'$m_S$ = %1.0f GeV' : 'ms',
+           '$m_1 = $ %1.0f GeV' : 'm1',
+        #    '$m_0 = $ %1.2f GeV' : 'm0',
+           '$\\Delta m_{10} = $ %1.0f GeV' : 'dm',
+           '$c \\tau = $ %1.0f m' : 'ctau'}
+
+axisDict = {'$m_S$ (GeV)' : 'ms',
+           '$m_1$ (GeV)' : 'm1',
+        #    '$m_0 = $ %1.2f GeV' : 'm0',
+           '$\\Delta m_{10}$ (GeV)' : 'dm',
+           '$c \\tau (m)$' : 'ctau',
+           '$\\epsilon_{\\rm T}$' : 'eff',
+           '$Z_{\\rm exp} = \\frac{n_s}{\sigma_b}$' : 'Z0',
+           '$\sigma_{\\rm UL}^{\\rm exp} (fb)$' : 'sigmaUL',
+           '$n_{s}$' : 'ns'}
+
+varDict = {val : key for key,val in axisDict.items()}
+
+def getTitleFromDF(df):
+
+    title = []
+    for texLabel,label in texDict.items():
+        if len(df[label].unique()) > 1:
+            continue
+        val = df[label].unique()[0]
+        title.append((texLabel % val))
+
+    return r', '.join(title)
+
+def getAxisLabel(df):
+    
+    axVars = []
+    for texLabel,label in axisDict.items():
+        if len(df[label].unique()) > 1:
+            axVars.append(texLabel)
+            
+    if len(axVars) == 1:
+        return (r'%s' %axVars[0])
+    else:
+        return 'x(?)'
 
 def getModelDict(inputFile,model='minimalH',verbose=True,bannerFile=None):
 
